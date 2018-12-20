488bea90d9dd13a0049d04c9890fc736d0fd06dd
mspenski <44811974+mspenski@users.noreply.github.com>
2018-12-19 21:20:19 -0500

    created jumbotron for main section and did some sizing and positioning for it. ready to put in questions and answers

---
+++ assets/javascript/app.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+    var options = [
+        {
+            question: "Pupusas, handmade thick stuffed corn tortillas, are a traditional dish from what country?", 
+            choice: ["Ethiopia", "El Salvadore", "Peru", "Guatamala"],
+            answer: 1,
+            photo: "assets/images/pupusas.jpg"
+         },
+         {
+             question: "What popular soda beverage was originally developed as a mixer for whiskey?", 
+            choice: ["Mountain Dew", "Sprite", "7-UP", "Coke"],
+            answer: 0,
+            photo: "assets/images/mtdew.gif"
+         }, 
+         {
+             question: "Kopi luwak is a very expensive type of what?", 
+            choice: ["Spice", "Caviar", "Coffee", "Rice variety" ],
+            answer: 2,
+            photo: "assets/images/coffee.gif"
+        }, 
+        {
+            question: "Which is not an ingredient in a Harvey Wallbanger cocktail?", 
+            choice: ["Orange Juice", "Vodka", "Sour Mix", "Galliano" ],
+            answer: 2,
+            photo: "assets/images/harvey.jpg"
+        }, 
+        {
+            question: "How many items are there in a Bakers' Dozen?", 
+            choice: ["12", "6", "24", "13" ],
+            answer: 3,
+            photo: "assets/images/dozen.jpg"
+        }, 
+        {
+            question: "What is the most widely eaten fish in the world?", 
+            choice: ["Tilapia", "Herring", "Sardine", "Tuna" ],
+            answer: 1,
+            photo: "assets/images/herring.jpg"
+        }, 
+        {
+            question: "Which fruit does not ripen once it has been picked?", 
+            choice: ["Banana", "Lemon", "Mango", "Apple" ],
+            answer: 1,
+            photo: "assets/images/lemon.gif"
+        }, 
+        {
+            question: "Which fruit contains the most protein per 100 calories?", 
+            choice: ["Guava", "Avocado", "Banana", "Blackberries" ],
+            answer: 0,
+            photo: "assets/images/guava.gif"
+        }];
+})
